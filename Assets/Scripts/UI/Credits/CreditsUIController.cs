3c6587814caf144302151fb20aae5de6ff79ddf6
JosephZKhan <josephzachariah.khan@gmail.com>
2026-06-26 14:26:29 +0100

    title and credits page info

--- Assets/Scripts/UI/Credits/CreditsUIController.cs
+++ Assets/Scripts/UI/Credits/CreditsUIController.cs
@@ -20,6 +20,11 @@ namespace JJ26.UI
 		public override void SetActive(bool active)
 		{
 			base.SetActive(active);
+
+			if(active)
+			{
+				ToggleCredits(false);
+			}
 		}
 
 		public override void UpdateController()
@@ -33,6 +38,13 @@ namespace JJ26.UI
 
 			UpdateInput();
 		}
+
+		[SerializeField] GameObject _infoGO;
+		[SerializeField] GameObject _creditsGO;
+
+		[SerializeField] TMP_Text _toggleCreditsText;
+
+		private bool _creditsActive;
 
 
 		#endregion //UIController
@@ -48,6 +60,21 @@ namespace JJ26.UI
 		public void Input_BackButtonPressed()
 		{
 			UIStateSystem.EnterPreviousScreen();
+		}
+
+		public void Input_ToggleCreditsPressed()
+		{
+			ToggleCredits(!_creditsActive);
+		}
+
+		public void ToggleCredits(bool active)
+		{
+			_infoGO.SetActive(!active);
+			_creditsGO.SetActive(active);
+
+			_toggleCreditsText.text = active ? "Info" : "Credits";
+
+			_creditsActive = active;
 		}
 	}
 }
